5d2defb884f01186e562a66e661500598a51f9f9
Elliot Huffman <thelich2@gmail.com>
2015-09-03 13:38:04 -0400

    Initial creation
    Initial creation of the file with base code for fixing Windows Update.

---
+++ WU Fix.bat
@@ -0,0 +1,68 @@
+:start
+net stop bits
+net stop wuauserv
+net stop appidsvc
+net stop cryptsvc
+Del "%ALLUSERSPROFILE%\Application Data\Microsoft\Network\Downloader\qmgr*.dat"
+set /p menu=Press 1 first time, or press 2 to powerwash Windows Update:
+if %menu%==1 goto next
+if %menu%==2 goto powerwash
+cls
+echo You have not entered a correct response
+echo please try again
+pause
+goto start
+
+:powerwash
+Ren %systemroot%\SoftwareDistribution SoftwareDistribution.bak
+Ren %systemroot%\system32\catroot2 catroot2.bak
+sc.exe sdset bits D:(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWLOCRRC;;;AU)(A;;CCLCSWRPWPDTLOCRRC;;;PU)
+sc.exe sdset wuauserv D:(A;;CCLCSWRPWPDTLOCRRC;;;SY)(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;BA)(A;;CCLCSWLOCRRC;;;AU)(A;;CCLCSWRPWPDTLOCRRC;;;PU)
+
+:next
+cd /d %windir%\system32
+regsvr32.exe atl.dll
+regsvr32.exe urlmon.dll
+regsvr32.exe mshtml.dll
+regsvr32.exe shdocvw.dll
+regsvr32.exe browseui.dll
+regsvr32.exe jscript.dll
+regsvr32.exe vbscript.dll
+regsvr32.exe scrrun.dll
+regsvr32.exe msxml.dll
+regsvr32.exe msxml3.dll
+regsvr32.exe msxml6.dll
+regsvr32.exe actxprxy.dll
+regsvr32.exe softpub.dll
+regsvr32.exe wintrust.dll
+regsvr32.exe dssenh.dll
+regsvr32.exe rsaenh.dll
+regsvr32.exe gpkcsp.dll
+regsvr32.exe sccbase.dll
+regsvr32.exe slbcsp.dll
+regsvr32.exe cryptdlg.dll
+regsvr32.exe oleaut32.dll
+regsvr32.exe ole32.dll
+regsvr32.exe shell32.dll
+regsvr32.exe initpki.dll
+regsvr32.exe wuapi.dll
+regsvr32.exe wuaueng.dll
+regsvr32.exe wuaueng1.dll
+regsvr32.exe wucltui.dll
+regsvr32.exe wups.dll
+regsvr32.exe wups2.dll
+regsvr32.exe wuweb.dll
+regsvr32.exe qmgr.dll
+regsvr32.exe qmgrprxy.dll
+regsvr32.exe wucltux.dll
+regsvr32.exe muweb.dll
+regsvr32.exe wuwebv.dl
+netsh winsock reset
+
+:end
+net start bits
+net start wuauserv
+net start appidsvc
+net start cryptsvc
+echo Press any key to reboot" | pause
+shutdown /r /t 00
